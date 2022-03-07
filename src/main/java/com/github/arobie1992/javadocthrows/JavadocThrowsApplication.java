889dc1ee58b753e309393884d491cdf72fd3a886
Andrew Robie <arobie1992@yahoo.com>
2022-03-07 17:56:32 -0500

    Initial project setup

---
+++ src/main/java/com/github/arobie1992/javadocthrows/JavadocThrowsApplication.java
@@ -0,0 +1,13 @@
+package com.github.arobie1992.javadocthrows;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavadocThrowsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavadocThrowsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/arobie1992/javadocthrows/JavadocThrowsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.arobie1992.javadocthrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavadocThrowsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
